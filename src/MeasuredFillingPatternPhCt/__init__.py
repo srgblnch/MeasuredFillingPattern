2513a4c8e1ebd3f1564c156b9fb453bcae49bfec
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2014-07-16 08:59:38 +0000

    first release where the analysis with PhCt works
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-ds/code/DeviceClasses/Calculation/MeasuredFillingPattern/branches/PhotonCounterClass@18071 3b39871e-fe4b-0410-b5db-e82997f68959

--- src/MeasuredFillingPatternPhCt/__init__.py
+++ src/MeasuredFillingPatternPhCt/__init__.py
@@ -1,2 +1,1 @@
 from MeasuredFillingPatternPhCt import *
-
--- src/MeasuredFillingPatternPhCt/phAnalyser.py
+++ src/MeasuredFillingPatternPhCt/phAnalyser.py
@@ -58,29 +58,131 @@ from copy import copy, copy
 from scipy import signal
 import time
 import taurus
+import PyTango
 
-class PhCtAnalyzer:
-    def __init__(self):
-        pass
+class Analyser:
+    def __init__(self,parent=None):
+        self._parent = parent
+
+    ######
+    #----# auxiliary methods for logging
+    def info(self,msg):
+        try:
+            if self._parent:
+                self._parent.info_stream(msg)
+            else:
+                print("info: %s"%(msg))
+        except: print("cannot print in info stream (%s)"%msg)
+    def debug(self,msg):
+        try:
+            if self._parent:
+                self._parent.debug_stream(msg)
+            else:
+                print("debug: %s"%(msg))
+        except: print("cannot print in debug stream (%s)"%msg)
+    def warn(self,msg):
+        try:
+            if self._parent:
+                self._parent.warn_stream(msg)
+            else:
+                print("warn:  %s"%(msg))
+        except: print("cannot print in warn stream (%s)"%msg)
+    def error(self,msg):
+        try:
+            if self._parent:
+                self._parent.error_stream(msg)
+            else:
+                print("error: %s"%(msg))
+        except: print("cannot print in error stream (%s)"%msg)
+    # done logging section
+    ######
     
+    ######
+    #----# auxiliary methods to manage events
+    def subscribe_event(self,attrName):
+        self._AttrEvent = self._PhCtDevProxy.subscribe_event(attrName,
+                                                PyTango.EventType.CHANGE_EVENT,
+                                                                          self)
+    def unsubscribe_event(self,devName):
+        self._PhCtDevProxy.unsubscribe_event(self._AttrEvent)
+        self._parent.change_state(PyTango.DevState.OFF)
+    #---- auxiliary methods to manage events
+    ######
+
+class PhCtAnalyzer(Analyser):
+    def __init__(self,PhCtDevName,
+                 histogram="histogram",resolution="resolution",
+                 BucketLenght=2*1e-9,
+                 parent=None):
+        Analyser.__init__(self, parent)
+        self._PhCtDevName = PhCtDevName
+        self._PhCtDevProxy = PyTango.DeviceProxy(self._PhCtDevName)
+        self._Histogram = PhCtDevName+"/"+histogram
+        self._Resolution = PhCtDevName+"/"+resolution
+        self._BucketLength = BucketLenght
+        #for the logging
+        self._parent = parent
+    
+    #a callback method for the scope channel attribute
+    def push_event(self,event):
+        try:
+            if event != None:
+                if event.device.dev_name() == self._PhCtDevName:
+                    if event.attr_value != None and \
+                       event.attr_value.value != None:
+                        if event.attr_value.quality == \
+                                                PyTango.AttrQuality.ATTR_VALID:
+                            self.info("Received valid data! (%d)"
+                                      %(len(event.attr_value.value)))
+                            bucket,fil_pat = self.Fil_Pat_Calc(\
+                                                        event.attr_value.value)
+                            self.emit_results(fil_pat)
+                        else:
+                            self.debug("Data is changing")
+                    else:
+                        self.debug("PushEvent() %s: value has None type"
+                                   %(event.attr_name))
+                else:
+                    self.warn("Received an unexpected event from %s"
+                              %(event.device.dev_name()))
+            else:
+                self.warn("Received a null event")
+        except Exception,e:
+            self.error("cannot process event due to: %s"%e)
+    def emit_results(self,fillingPattern):
+        if self._parent:
+            self._parent.fireEventsList([['BunchIntensity',
+                                          fillingPattern,
+                                          PyTango.AttrQuality.ATTR_VALID]])
+            self._parent.attr_BunchIntensity_read = fillingPattern
+
     ####
     # original methods of the ph analysis
-    def Fil_Pat_Calc(self,Time_Window, x_data, y_data, Secperbin):
+    def Fil_Pat_Calc(self,y_data):
         '''Calculation of the filling status of the 448 buckets'''
+        self.debug("Fil_Pat_Calc()")
+        # Usefull variables
+        self._secperbin = taurus.Attribute(self._Resolution).read().value *1e-12
+        #Convert the resolution (ps) in second
+        self._time_win = int(self._BucketLength/self._secperbin)
+        self._Tot_Bucket = int(448*self._BucketLength/self._secperbin)
+        #prepare arrays
+        y_data = y_data[0:self._Tot_Bucket+1]
+        x_data = range(len(y_data))
         fil_pat = [] 
         k = 0 
         Start = 0
         i=0
         #Analysis
-        print "Data analysis"
+        self.debug("Data analysis")
         while (Start < len(y_data)):
             k = 0
             time_win_ar = [] #Array representing the time of a bucket
-            if (Start + Time_Window < len(y_data)):
-                for k in range(0, Time_Window): 
+            if (Start + self._time_win < len(y_data)):
+                for k in range(0, self._time_win): 
                     time_win_ar.append(y_data[Start+k]) #create the bucket
                 fil_pat.append(sum(time_win_ar)) #considering all the photons in the bucket
-            Start = Start + Time_Window #switch to the following bucket
+            Start = Start + self._time_win #switch to the following bucket
         #Impose a threshold (Not sure if needed)
         i = 0
         Max = max(fil_pat)
@@ -99,15 +201,42 @@ class PhCtAnalyzer:
         return (bucket,fil_pat)
     # done original methods of the ph analysis
     ####
+    
+    ######
+    #----# auxiliary methods for logging
+    def info(self,msg):
+        try:
+            if self._parent:
+                self._parent.info_stream(msg)
+            else:
+                print("info: %s"%(msg))
+        except: print("cannot print in info stream (%s)"%msg)
+    def debug(self,msg):
+        try:
+            if self._parent:
+                self._parent.debug_stream(msg)
+            else:
+                print("debug: %s"%(msg))
+        except: print("cannot print in debug stream (%s)"%msg)
+    def warn(self,msg):
+        try:
+            if self._parent:
+                self._parent.warn_stream(msg)
+            else:
+                print("warn:  %s"%(msg))
+        except: print("cannot print in warn stream (%s)"%msg)
+    def error(self,msg):
+        try:
+            if self._parent:
+                self._parent.error_stream(msg)
+            else:
+                print("error: %s"%(msg))
+        except: print("cannot print in error stream (%s)"%msg)
+    # done logging section
+    ######
+    
 # Done PhCtBunchAnalyser Class
 ####
-
-#Functions definition
-
-# 
-
-
-
 
 ####
 # plot methods used when this is called by command line
@@ -124,24 +253,10 @@ def plotPhCt(bucket,fil_pat):
 ####
 
 def main():
-
-    # Usefull variables
-    
-    secperbin = taurus.Attribute('bl34/di/phct-01/Resolution').read().value
-    secperbin = secperbin*1e-12   #Convert the resolution (ps) in second
-    BucketLength = 2*1e-9 # seconds
-    time_win = int(BucketLength/secperbin)   
-    Tot_Bucket = int(448*BucketLength/secperbin)
-
-    ################################################# Loading and averaging  data ########################################
-    
-    y = taurus.Attribute('bl34/di/phct-01/Histogram').read().value
-    y = y[0:Tot_Bucket+1]
-    x = range(len(y))
-    
     ################################################ Analysis ##############################################################
-    FP = PhCtAnalyzer()
-    bucket,fil_pat = FP.Fil_Pat_Calc(time_win, x, y, secperbin) #Final output
+    FP = PhCtAnalyzer('bl34/di/phct-01')
+    y = taurus.Attribute('bl34/di/phct-01/Histogram').read().value
+    bucket,fil_pat = FP.Fil_Pat_Calc(y) #Final output
     plotPhCt(bucket,fil_pat)
     
     ################################################ Output #################################################################
